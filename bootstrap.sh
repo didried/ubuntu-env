fe0b4e68e996c2276b14a1498223ef2098a462d0
Edgars Didrihsons <edgars.didrihsons@gmail.com>
2016-11-18 14:17:55 +0200

    Add virtualbox in the list of installed software

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
 # install ansible
+echo installing ansible
 apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get install -y software-properties-common ansible
+
+# install required roles
+echo
+echo installing required ansible roles
+ansible-galaxy install -r requirements.yml -p roles/
+
+# install everything else
+echo
+echo installing everything
+ansible-playbook ubuntu-env.yml -i localhost, -c local -K -v
+
